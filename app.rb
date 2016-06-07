9a8faedd3f473c5b4ce22132f463fa603486c441
anthonyschurz <anthony.schurz@gmail.com>
2016-06-06 18:43:34 -0700

    chugging along

--- app.rb
+++ app.rb
@@ -3,10 +3,10 @@ class TrumpApp < Sinatra::Base
   get '/' do
     erb :layout
   end
-  
+
   post '/' do
-       # p params
        @name = Nickname.create(params[:name])
        @name.save
   end
+  
 end
