12b7806c634873c4cdbfdcd11a5153aa6529275d
anthonyschurz <anthony.schurz@gmail.com>
2016-06-09 09:55:17 -0700

    added some jquery

--- public/scripts.js
+++ public/scripts.js
@@ -1,17 +1,35 @@
 // jquery
 
+// Opponent image hover event
+
 $( '.wrapper' ).hover(function () {
     $( '.glyphicon-plus', this ).removeClass( 'hidden' );
 }, function () {
     $( '.glyphicon-plus', this ).addClass( 'hidden' );
 });
 
+// Add row to table
+
+$('#newhname').keypress(function (e) {
+ var key = e.which;
+ if(key == 13)  // the enter key code
+ {
+   $('#hillary-table > tbody > tr:first').before('<tr><td>column 1 value</td><td><input value="column two" readonly></td><td><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></td></tr>');
+ }
+});
+
+
+
+
+// Remove row from table
 
 $( 'table' ).on('click','.glyphicon-remove',function(e){
   e.preventDefault();
   $( this ).closest( 'tr' ).remove();
 });
 
+
+// Edit row in table
 
 $( 'table' ).on('click','.glyphicon-pencil',function(e){
   e.preventDefault();
